a34fcce7aed8b9a4afb5253dbf25ff1d43efdcc6
hug-delphi <hug-delphi@outlook.com>
2025-07-21 12:03:54 +0700

    Initial commit for Render API

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const router = express.Router()
+
+module.exports = router
+
+router.get('/', (req, res) => {
+  res.send({
+    result: 'success',
+    message: 'api'
+  })
+})
+/*
+router.use('/expOveSell', require('./expOveSell')) // export oversea selling
+router.use('/deliOrd', require('./deliOrd')) // delivery order
+router.use('/invoice', require('./invoice')) // invoice
+router.use('/forecast', require('./forecast')) // forecast
+router.use('/custPriceList', require('./custPriceList')) // customer price list
+router.use('/monCustOrd', require('./monCustOrd')) // Monthly Customer Order List By Duedate (Report)
+router.use('/system', require('./system')) // get running number
+router.use('/items', require('./items')) // Item management (CRUD)
+router.use('/orderPickList', require('./orderPickList'))
+router.use('/itemcrossref', require('./itemcrossref'))
+router.use('/customerorder', require('./customerorder'))
+router.use('/customer', require('./customer'))
+router.use('/report', require('./report'))
+router.use('/coSoldPartList', require('./coSoldPartList'))
+router.use('/users', require('./users'))
+router.use('/compForecast', require('./compForecast'))
+router.use('/monSalAnaByItem', require('./monSalAnaByItem')) // Monthly Sales Analysis By Item Report
+router.use('/purchase', require('./purchase'))
+router.use('/baseUnit', require('./baseUnit'))
+router.use('/demo', require('./demo'))
+*/
